7293bd46ba8ff72e89d25b4b48e50aad33507752
3DUTEX <t3xdev@gmail.com>
2024-08-02 14:14:22 -0300

    [test] add test for helper AES Encryption

---
+++ Tests/AESEncryptionTests.cs
@@ -0,0 +1,31 @@
+using Application.Providers;
+using FluentAssertions;
+using Xunit.Abstractions;
+
+namespace Tests;
+
+public class AESEncryptionTests
+{
+    private readonly ITestOutputHelper _logger;
+
+    public AESEncryptionTests(ITestOutputHelper logger)
+    {
+        _logger = logger;
+    }
+
+    [Fact]
+    public void TestEncryptAndDecrypt()
+    {
+        var content = "teste";
+
+        var contentEncrypted = AESEncryptionHelper.Encrypt(content);
+
+        _logger.WriteLine($"contentEncrypted: {contentEncrypted}");
+
+        var contentDecrypted = AESEncryptionHelper.Decrypt(contentEncrypted);
+
+        // Assertions
+        contentEncrypted.Should().NotBeEmpty();
+        contentDecrypted.Should().Be("teste");
+    }
+}
